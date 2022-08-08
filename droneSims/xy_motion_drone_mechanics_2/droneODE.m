2fe74561b6c9aec82311a875b0dc001797d361a3
Andrew North <anwnorth@gmail.com>
2022-08-08 13:35:57 -0400

    Added Linearized EOM for control to xy_direction_sim.m & change droneODE.m inputs to U

--- droneSims/xy_motion_drone_mechanics_2/droneODE.m
+++ droneSims/xy_motion_drone_mechanics_2/droneODE.m
@@ -1,9 +1,13 @@
-function dxdt = droneODE(t,x,fThrust,psi,m1,m2,radius,height,L1,L3)
+function dxdt = droneODE(t,x,U1,U2,m1,m2,radius,height,L1,L3)
+k = 4E-7; % F_thrust = k*(motor angular veloctiy)
+fThrust = U1(1)*k; % Input 1, thrust force
+psi = U2(2); % Input 2, servo angle psi
 
 % Parameters
-rho = 1.225; % kg/m^3 density of the air. Using density at sea level.
-C_d = 0.5; % coefficient of drag (approximated 0.5)
+airDensity = 1.225; % kg/m^3 density of the air. Using density at sea level.
+drag1 = 0.5; % coefficient of drag (approximated 0.5)
 A = 0.3*0.09; % m^2 cross sectional area of drone
+drag2 = 0.5*airDensity*drag1*A; % second coefficient of drag to simplify below eqns 
 g = 9.81;
 M = m1+m2;
 Icm = (M/48)*(3*(radius*2)^2+4*height^2); % mass moment of intertia of solid cylinder
--- droneSims/xy_motion_drone_mechanics_2/xy_direction_sim.m
+++ droneSims/xy_motion_drone_mechanics_2/xy_direction_sim.m
@@ -11,6 +11,12 @@ M = m1+m2; % kg
 radius = 0.4; % m
 height = 1; % m
 
+% Parameters related to drag
+airDensity = 1.225; % kg/m^3 density of the air. Using density at sea level.
+drag1 = 0.5; % coefficient of drag (approximated 0.5)
+A = 0.3*0.09; % m^2 cross sectional area of drone
+drag2 = 0.5*airDensity*drag1*A; % second coefficient of drag to simplify below eqns
+
 % Input:
 psi = pi-(1*pi/180); % servo angle
 psi_degree = psi*180/pi;
@@ -26,22 +32,19 @@ phi_degree = phi*180/pi;
 
 % Calculate Forces
 fThrust = 10; % Newtons
-K1 = [1 -1 1 2 2 -2];
-K2 = [1 2 1 2 3 1];
 
 %% Numerical Integration
-tspan = [0 5];
-x0 = [0;0;0;0;0;0];
-xf = [2;0;1.5;0;0;0];
+% tspan = [0 5];
+% x0 = [0;0;0;0;0;0];
+% xf = [2;0;1.5;0;0;0];
 % [t,x] = ode45(@(t,x) droneODE(t,x,fThrust,psi,m1,m2,radius,height,L1,L3),tspan,x0);
-[t,x] = ode45(@(t,x) droneODE(t,x,-K1*x,-K2*x,m1,m2,radius,height,L1,L3),tspan,x0);
 
-plot(t,x(:,1),t,x(:,2),t,x(:,3),t,x(:,4),t,x(:,5))
-yline(0,'--')
-title('State vs. Time (\psi = 174^{\circ}, F_{thrust} = 12N)');
-xlabel('time (seconds)');
-ylabel('position (meters), velocity (m/s)');
-legend('x','xDot','y','yDot','\lambda');
+% plot(t,x(:,1),t,x(:,2),t,x(:,3),t,x(:,4),t,x(:,5))
+% yline(0,'--')
+% title('State vs. Time (\psi = 174^{\circ}, F_{thrust} = 12N)');
+% xlabel('time (seconds)');
+% ylabel('position (meters), velocity (m/s)');
+% legend('x','xDot','y','yDot','\lambda');
 
 
 g = 9.81;
@@ -52,15 +55,36 @@ d2 = (m1/M)*(L1+L3); % distance from m2 to drone center of mass
 di = radius*2; % drone diameter in meters
 hi = height; % drone height in meters
 I = (M/48)*(3*di^2+4*hi^2); % mass moment of intertia of solid cylinder
-% Linearized equations for control
-A = zeros(6);
-A(1,2) = 1;
-A(2,5) = -(fThrust/M)*sin(phi);
-A(3,4) = 1;
-A(4,5) = (fThrust/M)*cos(phi);
-A(5,6) = 1;
-A(6,5) = (1/I)*(-fG1*d1+fG2*d2);
-eig(A)
+
+% ---------- Linearized equations for control ---------- %
+% Parameters
+k = 4E-7; % F_thrust = k*(motor angular veloctiy)
+
+A = [0 1 0 0 0 0;...
+    0 (-0.5*drag2)/M 0 0 0 0;...
+    0 0 0 1 0 0;...
+    0 0 0 (-0.5*drag2)/M 0 0;...
+    0 0 0 0 0 1;...
+    0 0 0 0 (g/I)*(m2*d2-m1*d1) 0];
+
+% Replaced X(5) with pi (replaced lambda with pi)
+B = [0 0;...
+    -k*pi -(2/3)*k*5000^2;...
+    0 0;...
+    k -(2/3)*k*5000^2*pi;...
+    0 0;...
+    0 (2*k*d1)/(3*I)*5000^2];
+
+% eigs = [-1.1; -1.2; -1.3; -1.4; -1.5; -1.6];
+eigs = [-.2; -1.2; 0; -1.4; -1.5; -1.6];
+K = place(A,B,eigs);
+
+%% Numerical Integration with controller for U
+tspan = [0 1];
+x0 = [0;0;0;0;0;0];
+xf = [2;0;2;0;0;0];
+[t,x] = ode45(@(t,x) droneODE(t,x,-K*(x-xf),-K*(x-xf),m1,m2,radius,height,L1,L3),tspan,x0);
+
 
 % Call simulation
 for k=1:length(t)
